9d979c6a1c5db483af205661e27b69e3ebc115f1
Arman Harutyunyan <armanharut98@gmail.com>
2024-12-04 03:58:34 +0400

    change marketing lambda

--- lambdas/marketing/index.js
+++ lambdas/marketing/index.js
@@ -1,6 +1,5 @@
 exports.handler = async (event) => {
     console.log("Marketing Lambda triggered!")
-    const message = event.records[0].body.message
-    console.log(`Message: ${message}`)
+    console.log(event)
     return event
 }
